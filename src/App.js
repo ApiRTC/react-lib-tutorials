cefe6d3462e45fb76e942ce3d4653f8a56ca4e77
Romain Vailleux <romain.vailleux@apizee.com>
2023-01-23 17:55:54 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useSession, useConversation, useCameraStream, useConversationStreams, VideoStream } from '@apirtc/react-lib'
+import './App.css';
 function App() {
+
+  
+const { session } = useSession({ apiKey: 'myDemoApiKey' });
+const { localStream } = useCameraStream(session);
+const { conversation } = useConversation(session, 'conversationName', undefined, true);
+const { publishedStreams, subscribedStreams } = useConversationStreams(
+  conversation, localStream ? [{ stream: localStream }] : []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="mememe-container">
+
+        <h2>Local Streams</h2>
+        { <VideoStream stream={localStream} muted={true}></VideoStream> }
+        <video src={localStream?.getData()}></video>
+        </div>
+        <div id='remote-streams-container'>
+          <h2>Remote Streams</h2>
+          { 
+            subscribedStreams.map( (stream, index) =>{
+              return (<VideoStream stream={stream} muted={false} key={index}></VideoStream>)
+            })
+          }
+        </div>
       </header>
     </div>
   );
