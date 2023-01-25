95058fc541564fb25d61af36de68563e6fcbab49
Romain Vailleux <romain.vailleux@apizee.com>
2023-01-25 15:19:04 +0100

    removed publishedStreams

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 
 import { useSession, useConversation, useCameraStream, useConversationStreams, VideoStream } from '@apirtc/react-lib'
 import './App.css';
+
 function App() {
 
-  
-const { session } = useSession({ apiKey: 'myDemoApiKey' });
-const { localStream } = useCameraStream(session);
+//Get a connection session to ApiRTC Video platform
+const { session } = useSession({ apiKey: 'INSERT_YOUR_API_KEY_HERE' }); // Get an account on cloud.apirtc.com and retrieve your Api Key.
+
+//Grab the local camera video and audio stream
+const { stream: localStream } = useCameraStream(session);
+
+// Get the conversation stateful object
 const { conversation } = useConversation(session, 'conversationName', undefined, true);
-const { publishedStreams, subscribedStreams } = useConversationStreams(
-  conversation, localStream ? [{ stream: localStream }] : []);
+
+// Get the list of streams exchanged in the conversation
+const { subscribedStreams } = useConversationStreams(conversation, localStream ? [{ stream: localStream }] : []);
 
 
   return (
     <div className="App">
       <header className="App-header">
         <div id="mememe-container">
-
         <h2>Local Streams</h2>
-        { <VideoStream stream={localStream} muted={true}></VideoStream> }
-        <video src={localStream?.getData()}></video>
+        { localStream && <VideoStream stream={localStream} muted={true}></VideoStream> }
         </div>
         <div id='remote-streams-container'>
           <h2>Remote Streams</h2>
