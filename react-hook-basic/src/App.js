c1317797bf1cbe2bdf5583aa70a44bbf2ff94372
Romain Vailleux <romain.vailleux@apizee.com>
2023-01-25 15:25:10 +0100

    Revert "removed publishedStreams"
    This reverts commit 95058fc541564fb25d61af36de68563e6fcbab49.

--- react-hook-basic/src/App.js
+++ react-hook-basic/src/App.js
@@ -1,28 +1,24 @@
 
 import { useSession, useConversation, useCameraStream, useConversationStreams, VideoStream } from '@apirtc/react-lib'
 import './App.css';
-
 function App() {
 
-//Get a connection session to ApiRTC Video platform
-const { session } = useSession({ apiKey: 'INSERT_YOUR_API_KEY_HERE' }); // Get an account on cloud.apirtc.com and retrieve your Api Key.
-
-//Grab the local camera video and audio stream
-const { stream: localStream } = useCameraStream(session);
-
-// Get the conversation stateful object
+  
+const { session } = useSession({ apiKey: 'myDemoApiKey' });
+const { localStream } = useCameraStream(session);
 const { conversation } = useConversation(session, 'conversationName', undefined, true);
-
-// Get the list of streams exchanged in the conversation
-const { subscribedStreams } = useConversationStreams(conversation, localStream ? [{ stream: localStream }] : []);
+const { publishedStreams, subscribedStreams } = useConversationStreams(
+  conversation, localStream ? [{ stream: localStream }] : []);
 
 
   return (
     <div className="App">
       <header className="App-header">
         <div id="mememe-container">
+
         <h2>Local Streams</h2>
-        { localStream && <VideoStream stream={localStream} muted={true}></VideoStream> }
+        { <VideoStream stream={localStream} muted={true}></VideoStream> }
+        <video src={localStream?.getData()}></video>
         </div>
         <div id='remote-streams-container'>
           <h2>Remote Streams</h2>
